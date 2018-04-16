7de7676066ff842dd94dfd94aa0478fc7ab27ef7
Tim Cuthbertson <tim@gfxmonk.net>
2018-04-16 14:11:09 +1000

    0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import *
 here = os.path.dirname(__file__)
 setup(
 	name='stereoscoper',
-	version='0.3',
+	version='0.3.1',
 	author='Tim Cuthbertson',
 	scripts = ['stereoscoper'],
 	install_requires = ['pillow', 'numpy']
